7ec3ff9effc13b47d5a3e5b0eb0a2799a65167fe
DESKTOP-CJ5TJR4\MASENAK <peter736@uwm.edu>
2019-09-27 20:14:31 -0500

    made the skeleton for my console game. adding more classes if need be and content next.

---
+++ RPSLS/RPSLS/Game.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Game
+    {
+        // member variables (HAS A)
+
+        public Player playerOne;
+        public Player playerTwo;
+
+
+
+        // constructor (SPAWNER)
+        public Game()
+        {
+            playerOne = new Player();
+            playerTwo = new Player();
+
+        }
+
+
+
+        // member methods (CAN DO)
+        public void RunGame() //master method
+        {
+            DisplayRules();
+        }
+
+
+        public void DisplayRules()
+        {
+            Console.WriteLine("Two players take turns with Rock-Paper-Scissors-Lizard-Spock");
+            Console.WriteLine("Best of 3 determines the winner.");
+            Console.WriteLine("Rules:");
+            Console.WriteLine("Scissors cuts Paper");
+            Console.WriteLine("Scissors decapitates Lizard");
+            Console.WriteLine("Paper covers Rock");
+            Console.WriteLine("Paper disproves Spock");
+            Console.WriteLine("Rock crushes Lizard");
+            Console.WriteLine("Rock crushes Scissors ");
+            Console.WriteLine("Lizard eats Paper");
+            Console.WriteLine("Lizard poisons Spock");
+            Console.WriteLine("Spock smashes Scissors");
+            Console.WriteLine("Spock vaporizes Rock");
+        }
+    }
+}
---
+++ RPSLS/RPSLS/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Player
+    {
+        // member variables (HAS A)
+        public int score;
+        public string name;
+        public string gesture;
+
+
+
+        // consturctor (SPAWNER)
+        public Player()
+        {
+            score = 0;
+        }
+
+
+
+        // member methods (CAN DO)
+
+
+
+    }
+}
