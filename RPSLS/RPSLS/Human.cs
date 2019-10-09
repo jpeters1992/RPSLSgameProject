621d5a19b90ff59025285e2519c5f7e7058086c0
DESKTOP-CJ5TJR4\MASENAK <peter736@uwm.edu>
2019-10-09 08:31:46 -0500

    I built out the Human: Player gesture function

--- RPSLS/RPSLS/Human.cs
+++ RPSLS/RPSLS/Human.cs
@@ -16,6 +16,34 @@ namespace RPSLS
 
 
         //MEMBER METHODS
+        public void SelectGesture()
+        {
+            Console.WriteLine("Select one of the gestures: 'rock' 'paper' 'scissors' 'lizard' 'spock'");
+
+            switch (gesture)
+            {
+                case "rock":
+                    Console.WriteLine("You chose " + gesture);
+                    break;
+                case "paper":
+                    Console.WriteLine("You chose " + gesture);
+                    break;
+                case "scissors":
+                    Console.WriteLine("You chose " + gesture);
+                    break;
+                case "lizard":
+                    Console.WriteLine("You chose " + gesture);
+                    break;
+                case "spock":
+                    Console.WriteLine("You chose " + gesture);
+                    break;
+            }
+        }
+
+        public void SomeFunction()
+        {
+            //code execution 
+        }
 
     }
 }
--- RPSLS/RPSLS/Player.cs
+++ RPSLS/RPSLS/Player.cs
@@ -23,5 +23,8 @@ namespace RPSLS
         }
 
         //MEMBER METHODS
+
+        public abstract void SelectGesture();
+        public abstract void SomeFunction1();
     }
 }
