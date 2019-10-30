4d1615a44c68f909d37aa8eb23f3268910a4084c
DESKTOP-CJ5TJR4\MASENAK <peter736@uwm.edu>
2019-10-30 14:10:04 -0500

    making last changes for readability

--- RPSLS/RPSLS/Game.cs
+++ RPSLS/RPSLS/Game.cs
@@ -11,6 +11,7 @@ namespace RPSLS
         //MEMBER VARIABLES
         public Player playerOne;
         public Player playerTwo;
+        public int bestOfXrounds;
 
         //CONSTRUCTOR
         public Game()
@@ -23,22 +24,30 @@ namespace RPSLS
         {
             NewLine();
             DisplayRules();
-            NewLine();
+
             GetNumberOfPlayers();
-            NewLine();
+            ChooseBestOfXrounds();
             PlayRounds();
+        }
+
+        public static void NewLine()
+        {
+            //inserts blank line for spacing
+            Console.WriteLine("");
         }
 
         public void DisplayRules()
         {
-            System.Console.WriteLine("--Select a gesture - best of 3 rounds wins.-- \n--Rock crushes Scissors-- \n--Scissors cuts Paper-- \n--Paper covers Rock-- \n--Rock crushes Lizard-- \n--Lizard poisons Spock-- \n--Spock smashes Scissors-- \n--Scissors decapitates Lizard-- \n--Lizard eats Paper-- \n--Paper disproves Spock-- \n--Spock vaporizes Rock--");
+            System.Console.WriteLine("Select a gesture - best of X rounds wins: \n--Rock crushes Scissors-- \n--Scissors cuts Paper-- \n--Paper covers Rock-- \n--Rock crushes Lizard-- \n--Lizard poisons Spock-- \n--Spock smashes Scissors-- \n--Scissors decapitates Lizard-- \n--Lizard eats Paper-- \n--Paper disproves Spock-- \n--Spock vaporizes Rock--");
         }
 
         public void GetNumberOfPlayers()
+        
         {
             Console.WriteLine("Please enter the number of players: 1 or 2");
             string choice = Console.ReadLine();
            
+            
             if (choice == "1")
             {
                 playerOne = new Human();
@@ -55,23 +64,87 @@ namespace RPSLS
             }
         }
 
+        public void ChooseBestOfXrounds()
+        {
+            try
+            {
+                bestOfXrounds = Convert.ToInt32(UserInterface.RetrieveUserInput("How many rounds do you want? Best of X (odd numbers only)"));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please select an integer");
+            }
+
+            if (bestOfXrounds >= 3 && bestOfXrounds % 2 == 1)
+            {
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Please enter a number greater than or equal to 3");
+                ChooseBestOfXrounds();
+            }
+        }
+
         public void PlayRounds()
         {
             playerOne.SelectGesture();
             playerTwo.SelectGesture();
             GestureLogic();
+            AddScoreToWinner();
+            CheckRoundWinnerScore();
             Console.ReadLine();
         }
-
+// -----------------------------------------------------------------------------------------------
        
-        public void GestureLogic()
+        public bool GestureLogic()
         {
-            //code out
+            playerOne.isWinner = false;
+            playerTwo.isWinner = false;
+            if (playerOne.playerGesture.WinsAgainst(playerTwo.playerGesture))
+            {
+                return playerOne.isWinner = true;
+            }
+            else if (playerTwo.playerGesture.WinsAgainst(playerOne.playerGesture))
+            {
+                return playerTwo.isWinner = true;
+            }
+            else if ((!playerOne.playerGesture.WinsAgainst(playerTwo.playerGesture) && !playerTwo.playerGesture.WinsAgainst(playerOne.playerGesture)))
+            {
+                Console.WriteLine("Tied! Go again!");
+            }
+            return false;
         }
 
-        public static void NewLine()
+        public void AddScoreToWinner()
         {
-            Console.WriteLine("");
+            if (playerOne.isWinner)
+            {
+                playerOne.playerScore++;
+            }
+            else if (playerTwo.isWinner)
+            {
+                playerTwo.playerScore++;
+            }
+            return;
+        }
+
+        public void CheckRoundWinnerScore()
+        {
+            if (playerOne.playerScore == (bestOfXrounds + 1) / 2)
+            {
+                Console.WriteLine(playerOne.playerName + " has won the match!");
+            }
+            else if (playerTwo.playerScore == (bestOfXrounds + 1) / 2)
+            {
+                Console.WriteLine(playerTwo.playerName + " has won the match!");
+            }
+            Console.WriteLine("Current score: \n" + playerOne.playerName + " " + playerOne.playerScore + " - " + playerTwo.playerName + " " + playerTwo.playerScore);
+            while (playerOne.playerScore != (bestOfXrounds + 1) / 2 && playerTwo.playerScore != (bestOfXrounds + 1) / 2)
+            {
+                PlayRounds();
+                Console.ReadLine();
+            }
         }
     }
 }
